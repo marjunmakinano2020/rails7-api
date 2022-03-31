f92d1001c16146b4acb3ddf576d11cb9ebc30405
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-03-31 20:39:20 +0800

    Added resource using: rails g resource Trail name state

---
+++ forest-trails/db/migrate/20220331123819_create_trails.rb
@@ -0,0 +1,10 @@
+class CreateTrails < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trails do |t|
+      t.string :name
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
