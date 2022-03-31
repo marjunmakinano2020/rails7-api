101121eb08ab026411e85945b0331c3f2127ee05
Marjun Makinano <marjunmakinanotwentytwenty@gmail.com>
2022-03-31 21:12:44 +0800

    Updated seeds

--- forest-trails/db/seeds.rb
+++ forest-trails/db/seeds.rb
@@ -8,3 +8,9 @@
 
 Forest.destroy_all
 Trail.destroy_all
+
+forest1 = Forest.create(name: "Black Forest", state: "Colorado")
+forest2 = Forest.create(name: "Olympic National Forest", state: "Washington")
+
+trail1 = Trail.create(name: "Section 16", forest: forest1)
+trail2 = Trail.create(name: "Hall of Moss", forest: forest2)
